9a27645812946143aed756cae67aaa651fc8f741
Bice93 <bianca.cerulo@hotmail.it>
2023-02-15 16:56:02 +0100

    Added filter

--- src/main/java/com/example/pizzeria/controller/PizzaController.java
+++ src/main/java/com/example/pizzeria/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package com.example.pizzeria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.pizzeria.model.Pizza;
 import com.example.pizzeria.repository.PizzaRepository;
@@ -19,18 +21,40 @@ public class PizzaController {
 	@Autowired
 	private PizzaRepository pizzaRepository;
 	
-	@GetMapping
-	public String index(Model m) {
-		List<Pizza> elencoPizze = pizzaRepository.findAll();
-		m.addAttribute("pizze", elencoPizze);
-		return "/pizze/index";
-	}
+//	@GetMapping
+//	public String index(Model m) {
+//		List<Pizza> elencoPizze = pizzaRepository.findAll();
+//		m.addAttribute("pizze", elencoPizze);
+//		return "/pizze/index";
+//	}
 	
+	@GetMapping
+	public String index(@RequestParam(name="nome", required = false) String keyword, Model model) {
+		List<Pizza> elencoPizze;
+		if ( keyword != null && !keyword.isEmpty() ) {
+			elencoPizze = pizzaRepository.findByNomeLike("%" + keyword + "%");
+		} else {
+			elencoPizze = pizzaRepository.findAll();
+		}
+		model.addAttribute("pizze", elencoPizze);
+		return "pizze/index";
+	}
 	
 	@GetMapping("/{id}") // gestirà le richieste GET di tipo /pizze/id
 	public String detail(@PathVariable("id") Integer id, Model model) {
-		Pizza pizza = pizzaRepository.getReferenceById(id);
-		model.addAttribute("pizza", pizza);
+		
+		Optional<Pizza> pizza = pizzaRepository.findById(id);
+		if (pizza.isEmpty()) {
+			// return error
+		} else {
+			model.addAttribute("pizza", pizza.get());
+		}
+//		Pizza pizza = pizzaRepository.getReferenceById(id);
+//		model.addAttribute("pizza", pizza);
+		
 		return "pizze/detail";
 	}
+	
+	
+
 }
--- src/main/java/com/example/pizzeria/repository/PizzaRepository.java
+++ src/main/java/com/example/pizzeria/repository/PizzaRepository.java
@@ -1,9 +1,12 @@
 package com.example.pizzeria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.pizzeria.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-
+	//Query custom
+	public List<Pizza> findByNomeLike (String keyword);
 }

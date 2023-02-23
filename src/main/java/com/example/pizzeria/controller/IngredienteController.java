cf426a28ddb23205c3d1e04a112b0e6cad892ff5
Bice93 <bianca.cerulo@hotmail.it>
2023-02-23 15:29:26 +0100

    Create ingredient

--- src/main/java/com/example/pizzeria/controller/IngredienteController.java
+++ src/main/java/com/example/pizzeria/controller/IngredienteController.java
@@ -2,10 +2,13 @@ package com.example.pizzeria.controller;
 
 import java.util.List;
 
+import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.pizzeria.model.Ingrediente;
@@ -23,6 +26,18 @@ public class IngredienteController {
 		List<Ingrediente> ingredientsList = ingredienteRepository.findAll();
 		model.addAttribute("ingredienti", ingredientsList);
 		return "/ingredienti/index";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("ingrediente", new Ingrediente());
+		return "/ingredienti/create";
+	}
+	
+	@PostMapping("/create")
+	public String store(@ModelAttribute("ingrediente") Ingrediente formIngrediente , Model model) {
+		ingredienteRepository.save(formIngrediente);
+		return "redirect:/ingredienti";
 	}
 	
 	
